5a8b26f279fcb2c32179e850bf90076dbc288475
Mark Johnston <markjdb@gmail.com>
2021-06-12 13:29:39 -0400

    Initial revision.

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+bhyve -c 2 -m 4G \
+    -s 0,hostbridge \
+    -s 1,lpc \
+    -s 2,virtio-blk,${_FREEBSD_VM_IMG} \
+    -l com1,stdio \
+    -l bootrom,/usr/local/share/uefi-firmware/BHYVE_UEFI.fd \
+    testvm
